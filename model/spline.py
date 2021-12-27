569f8ecbef347b3a2a00b23a48e7a3244f519653
emperorlu <emperorlu@hust.edu.cn>
2021-12-27 15:04:58 +0800

    [Debug] Add code

---
+++ model/spline.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import interpolate
+import matplotlib.pyplot as plt
+import sys
+
+a = int(sys.argv[1])  
+# b = int(sys.argv[2])
+
+X = np.linspace(1,a+1,a)
+print(X)
+y =  np.array([x  for x in np.random.randint(0,2,a)])
+print(y)
+
+sx=np.linspace(1,a+1,a+2)
+func2=interpolate.UnivariateSpline(X,y,s=8)
+sy=func2(sx)
+
+
+plt.plot(X,y,'.')
+plt.plot(sx,sy)
+plt.savefig("spline.png")
+plt.show()
