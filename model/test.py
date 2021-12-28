4bbb31247f4092da3056a98bc0b4d3dc884ecf91
emperorlu <emperorlu@hust.edu.cn>
2021-12-28 15:10:54 +0800

    [Debug] Add code

--- model/test.py
+++ model/test.py
@@ -1,7 +1,24 @@
+from bloom_filter import BloomFilter
+import tensorflow as tf 
+import tensorflow_hub as hub
 import numpy as np
+import os
+from scipy import optimize
+from tqdm.notebook import tqdm
+from sklearn.metrics import classification_report,accuracy_score
+import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from scipy import optimize
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Input, Embedding, Activation, Flatten, Dense
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Dropout
+from tensorflow.keras.models import Model
+from copy import deepcopy
+from tensorflow.keras.utils import to_categorical
+import plotly.graph_objects as go
+import plotly.io as pio
+import matplotlib.pyplot as plt
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def gen_data():
     fs = open("../data/test_input.txt", "r",encoding='utf-8')
@@ -31,26 +48,132 @@ def gen_data():
         data.append([X[i], y[i]])
     return data
 
-test_data = gen_data()
-print("1 test_data:",test_data[:3])
-print("length",len(test_data))
-test_texts = np.array([test_data[i][0] for i in range(len(test_data))])
-print("2 test_texts:",test_texts[:3])
-print("length",len(test_texts))
+data = gen_data()
+train_features = np.array([str(i[0]) for i in data])
+train_labels = np.array([i[1] for i in data])
+
 tk = Tokenizer(num_words=None, char_level=True, oov_token='UNK') 
-test_texts = [s.lower() for s in test_texts]
-print("3 test_texts:",test_texts[:3])
-print("length",len(test_texts))
-test_texts = tk.texts_to_sequences(test_texts)
-print("4 test_texts:",test_texts[:3])
-print("length",len(test_texts))
-data = pad_sequences(test_texts, maxlen=1014, padding='post')
-print("5 data:",test_texts[:3])
-print("length",len(data))
-data = np.array(data, dtype='float32')
-print("6 data:",test_texts[:3])
-print("length",len(data))
-# y =  model.predict(test_data)
+input_size = 1014
+
+embedding_size = 69
+conv_layers = [[256, 7, 3],
+            [256, 7, 3],
+            [256, 3, -1],
+            [256, 3, -1],
+            [256, 3, -1],
+            [256, 3, 3]]
+
+fully_connected_layers = [64, 4]
+num_of_classes = 2
+dropout_p = 0.1
+optimizer = 'adam'
+loss = 'categorical_crossentropy'
+embedding_weights = []
+data1 = deepcopy(data)
+data1 = np.array(data1)
+train_texts = data1[:,0]
+y_train = data1[:,1]
+train_texts = [s.lower() for s in train_texts]
+tk.fit_on_texts(train_texts)
+alphabet = "abcdefghijklmnopqrstuvwxyz0123456789,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+char_dict = {}
+for i, char in enumerate(alphabet):
+    char_dict[char] = i + 1
+
+tk.word_index = char_dict.copy()
+tk.word_index[tk.oov_token] = max(char_dict.values()) + 1
+train_texts = tk.texts_to_sequences(train_texts)
+
+# Padding
+train_data = pad_sequences(train_texts, maxlen=1014, padding='post')
+
+# Convert to numpy array
+train_data = np.array(train_data, dtype='float32')
+# test_data = np.array(test_data, dtype='float32')
+
+# =======================Get classes================
+# train_y = train_df[0].values
+train_class_list = [x  for x in y_train]
+
+train_classes = to_categorical(train_class_list)
+# test_classes = to_categorical(test_class_list)
+# tk = Tokenizer(num_words=None, char_level=True, oov_token='UNK') 
+
+# =====================Char CNN=======================
+# parameter
+
+
+# Embedding weights
+# (70, 69)
+vocab_size = len(tk.word_index)
+embedding_weights.append(np.zeros(vocab_size))  # (0, 69)
+
+for char, i in tk.word_index.items():  # from index 1 to 69
+    onehot = np.zeros(vocab_size)
+    onehot[i-1] = 1
+    embedding_weights.append(onehot)
+
+embedding_weights = np.array(embedding_weights)
+print('Load')
+
+# Embedding layer Initialization
+embedding_layer = Embedding(vocab_size + 1,
+                            embedding_size,
+                            input_length=input_size,
+                            weights=[embedding_weights])
+inputs = Input(shape=(input_size,), name='input', dtype='int64')  # shape=(?, 1014)
+# Embedding
+x = embedding_layer(inputs)
+# Conv
+# for filter_num, filter_size, pooling_size in conv_layers:
+x = Conv1D(64, 4)(x)
+x = Activation('relu')(x)
+# if pooling_size != -1:
+x = MaxPooling1D(pool_size=2)(x)  # Final shape=(None, 34, 256)
+x = Flatten()(x)  # (None, 8704)
+# Fully connected layers
+for dense_size in fully_connected_layers:
+    x = Dense(dense_size, activation='relu')(x)  # dense_size == 1024
+    x = Dropout(dropout_p)(x)
+# Output Layer
+predictions = Dense(num_of_classes, activation='softmax')(x)
+# Build model
+model = Model(inputs=inputs, outputs=predictions)
+model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])  # Adam, categorical_crossentropy
+model.summary()
+
+model.fit(train_data, train_classes,
+        batch_size=256,
+        epochs=1,
+        verbose=2)
+
+def test_model(test_texts):
+    print("3 test_texts:",test_texts[:3])
+    print("length",len(test_texts))
+    test_texts = tk.texts_to_sequences(test_texts)
+    print("4 test_texts:",test_texts[:3])
+    print("length",len(test_texts))
+    data = pad_sequences(test_texts, maxlen=1014, padding='post')
+    print("5 data:",data[:3])
+    print("length",len(data))
+    data = np.array(data, dtype='float32')
+    print("6 data:",data[:3])
+    print("length",len(data))
+    y =  model.predict(data)
+    print("7 y:",y[:3])
+    print("length",len(y))
+    ans =[]
+    for f in y:
+        ans.append(f[1])
+    return ans
+
+
+test_data = data.copy()
+y = np.array([i[1] for i in test_data])
+test_data = np.array([test_data[i][0] for i in range(len(test_data))])
+prediction = test_model(test_data)
+print("8 prediction:",prediction[:3])
+print("length",len(prediction))
 
 
 def f(x):
@@ -58,14 +181,3 @@ def f(x):
 
 minimum = optimize.fminbound(f, '2', '6')
 print("minimum:",minimum)
-#     test_texts = [s.lower() for s in test_texts]
-#     test_texts = tk.texts_to_sequences(test_texts)
-#     test_data = pad_sequences(test_texts, maxlen=1014, padding='post')
-#     test_data = np.array(test_data, dtype='float32')
-#     y =  model.predict(test_data)
-
-#     return y
-
-
-
-# minimum = optimize.fminbound(f, 'andysgame', 'ceskarepublika')
