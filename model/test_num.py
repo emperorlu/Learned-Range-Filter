6ec85664aacb3fc225a7c807dd33e3c494e2d193
emperorlu <emperorlu@hust.edu.cn>
2021-12-30 20:32:19 +0800

    [Debug] Add code

--- model/test_num.py
+++ model/test_num.py
@@ -75,10 +75,11 @@ train_classes = to_categorical(train_class_list)
 input_size = 14
 embedding_size = 3
 conv_layers = [[256, 7, 3],
-            # [256, 7, 3],
+            [256, 7, 3],
             [256, 3, -1],
-            # [256, 3, -1],
-            # [256, 3, -1],
+            [256, 3, -1],
+            [256, 3, -1],
+            [256, 3, -1],
             [256, 3, 3]]
 
 fully_connected_layers = [64, 4]
