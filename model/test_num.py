96f383088da693ef5ba72c76a1ab14af3e0c4dcb
emperorlu <emperorlu@hust.edu.cn>
2021-12-30 20:14:16 +0800

    [Debug] Add code

--- model/test_num.py
+++ model/test_num.py
@@ -109,23 +109,23 @@ inputs = Input(shape=(input_size,), name='input', dtype='int64')  # shape=(?, 10
 x = embedding_layer(inputs)
 
 # x = GRU(64)(x)
-x = LSTM(64)(x)
+# x = LSTM(64)(x)
 
-# # Conv
-# # for filter_num, filter_size, pooling_size in conv_layers:
-# x = Conv1D(64, 4)(x)
-# x = Activation('relu')(x)
-# # if pooling_size != -1:
-# x = MaxPooling1D(pool_size=2)(x)  # Final shape=(None, 34, 256)
-# x = Flatten()(x)  # (None, 8704)
-# # Fully connected layers
-# for dense_size in fully_connected_layers:
-#     x = Dense(dense_size, activation='relu')(x)  # dense_size == 1024
-#     x = Dropout(dropout_p)(x)
+# Conv
+# for filter_num, filter_size, pooling_size in conv_layers:
+x = Conv1D(64, 4)(x)
+x = Activation('relu')(x)
+# if pooling_size != -1:
+x = MaxPooling1D(pool_size=2)(x)  # Final shape=(None, 34, 256)
+x = Flatten()(x)  # (None, 8704)
+# Fully connected layers
+for dense_size in fully_connected_layers:
+    x = Dense(dense_size, activation='relu')(x)  # dense_size == 1024
+    x = Dropout(dropout_p)(x)
 
 
 # Output Layer
-predictions = Dense(num_of_classes, activation='relu')(x) #softmax
+predictions = Dense(num_of_classes, activation='softmax')(x) #softmax
 # Build model
 # optimizer = optimizers.Adam(learning_rate=0.1, decay=0.001)
 model = Model(inputs=inputs, outputs=predictions)
