bb55652165654901ef80798a62fe31e026506f1b
emperorlu <emperorlu@hust.edu.cn>
2021-12-30 19:55:59 +0800

    [Debug] Add code

--- model/test_num.py
+++ model/test_num.py
@@ -108,7 +108,7 @@ inputs = Input(shape=(input_size,), name='input', dtype='int64')  # shape=(?, 10
 # Embedding
 x = embedding_layer(inputs)
 
-x = GRU(100)(x)
+x = GRU(64)(x)
 
 # Conv
 # for filter_num, filter_size, pooling_size in conv_layers:
