f9a844fda96dfda5d732d4479981db34b52e0d7e
emperorlu <emperorlu@hust.edu.cn>
2021-12-30 20:23:36 +0800

    [Debug] Add code

--- model/test_num.py
+++ model/test_num.py
@@ -120,9 +120,9 @@ x = MaxPooling1D(pool_size=2)(x)  # Final shape=(None, 34, 256)
 x = Flatten()(x)  # (None, 8704)
 
 # Fully connected layers
-# for dense_size in fully_connected_layers:
-#     x = Dense(dense_size, activation='relu')(x)  # dense_size == 1024
-#     x = Dropout(dropout_p)(x)
+for dense_size in fully_connected_layers:
+    x = Dense(dense_size, activation='relu')(x)  # dense_size == 1024
+    # x = Dropout(dropout_p)(x)
 
 
 # Output Layer
