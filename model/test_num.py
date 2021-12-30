a87dc7b58acdb9477637d161381779c93b9ae967
emperorlu <emperorlu@hust.edu.cn>
2021-12-30 14:58:20 +0800

    [Debug] Add code

--- model/test_num.py
+++ model/test_num.py
@@ -75,12 +75,12 @@ train_data = np.array(train_data, dtype='float32')
 train_class_list = [x  for x in y_train]
 
 train_classes = to_categorical(train_class_list)
-# print("1 train_data:",train_data[:3])
-# print("length",len(train_data))
-# print("type",type(train_data))
-# print("2 train_classes:",train_classes[:3])
-# print("length",len(train_classes))
-# print("type",type(train_classes))
+print("1 train_data:",train_data[:3])
+print("length",len(train_data))
+print("type",type(train_data))
+print("2 train_classes:",train_classes[:3])
+print("length",len(train_classes))
+print("type",type(train_classes))
 
 # =====================Char CNN=======================
 # parameter
