d359cb6ef3da55bd7b9e431bbeeac54ea60ded9e
emperorlu <emperorlu@hust.edu.cn>
2021-12-31 12:31:24 +0800

    [Debug] Add code

--- model/test_num.py
+++ model/test_num.py
@@ -122,7 +122,7 @@ x = Flatten()(x)  # (None, 8704)
 
 # Fully connected layers
 for dense_size in fully_connected_layers:
-    x = Dense(dense_size, activation='relu')(x)  # dense_size == 1024
+    x = Dense(dense_size, activation='relu', kernel_initializer='random_normal')(x)  # dense_size == 1024
     x = Dropout(dropout_p)(x)
 
 
